60e153efe888c49ec7c2c6d493d3db10f045146c
David Staudt <dstaudt@cisco.com>
2020-01-06 19:13:54 -0600

    - Move attachmentActions event handling from Webhook to using the new websocket-based functionality in the Teams SDK - Move credentials into .env (from launch.json) - Refactor event handlers to consistently send `event` - Ignore membership create events for people other than the bot

--- index.js
+++ index.js
@@ -16,10 +16,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-// Sample Java program demonstrating usage of the Cisco WebDialer SOAP API
-// and Apache Axis to perform a <makeCallSoap> call, wait 5 seconds, then end the
-// call with <endCallSoap>
-
 // See README.md for configuration and launch instructions
 
 // Tested using:
@@ -27,22 +23,27 @@
 // Ubuntu Linux 19.04
 // Node.js 10.15.0
 
-const express = require('express');
-const webex = require('webex/env');
-const ngrok = require('ngrok');
-var request = require("request-promise-native");
-var bodyParser = require('body-parser');
+require( 'dotenv' ).config();
+const express = require( 'express' );
+const webex = require( 'webex/env' );
+const ngrok = require( 'ngrok' );
+var request = require( 'request-promise-native' );
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
-var publicUrl = '';
-var botId = '';
+var publicUrl, botId;
 
-async function sendStrategy( roomId ) {
+async function sendStrategy( event ) {
+
+    // Ignore memberships events for users other than our bot
+    if ( event.resource == 'memberships' && event.data.personId !== botId ) return;
+    // Ignore messages sent by our bot
+    if ( event.resource == 'messages' && event.data.personId == botId ) return;
 
     try {
 
+        // Retrieve the strategy from Oblique Strategies
         let html = await request( 'http://stoney.sb.org/eno/oblique.html' );
 
         // Scrape the strategy text out of the HTML page
@@ -51,15 +52,14 @@ async function sendStrategy( roomId ) {
     catch ( err ) {
 
         strategy = `Error retrieving strategy: ${ err }`;
-
-        console.log( strategy )
+        console.log( strategy );
     }
 
     message = {
-        "roomId": roomId,
-        "markdown": `"${ strategy }"`,
-        "attachments": [ ]
-    }
+        'roomId': event.data.roomId,
+        'markdown': `"${ strategy }"`,
+        'attachments': [ ]
+    };
 
     // Card attachment based on the schema at https://adaptivecards.io/explorer/
     // The WYSISWYG designer is helpful as well: https://adaptivecards.io/designer/
@@ -72,7 +72,7 @@ async function sendStrategy( roomId ) {
             "body": [
                 {
                     "type": "Image",
-                    "url": `${publicUrl}/images/oblique.png`
+                    "url": `${ publicUrl }/images/oblique.png`
                 },
                 {
                     "type": "TextBlock",
@@ -174,19 +174,36 @@ async function sendStrategy( roomId ) {
                 }			        
             ]
         }
-    }
+    };
 
     message.attachments.push( attachment );
 
     try {
 
-        await webex.messages.create( message )
+        await webex.messages.create( message );
     }
     catch ( err ) {
 
-        console.log( `Error creating message: ${ err }`)
+        console.log( `Error creating message: ${ err }`);
     }
+}
 
+async function handleAttachmentActions( event ) {
+
+    switch ( event.data.inputs.action ) {
+
+        case 'refresh': 
+
+            sendStrategy( event );
+            break;
+
+        case 'settings':
+
+            // Saving the settings and scheduling automatic strategy posts 
+            // is left as an exercise for the reader ;)
+            console.log( event.data.inputs )
+            break;
+    }
 }
 
 async function setupWebexListeners() {
@@ -195,116 +212,45 @@ async function setupWebexListeners() {
 
         await webex.memberships.listen();
 
-        // Send a sample strategy when we're added to a new room
-        webex.memberships.on( 'created', ( event ) => sendStrategy( event.data.roomId ) );
-
-        // Useful if implementing scheduled auto posts
-        webex.memberships.on( 'deleted', ( event ) => { } );
-    }
-    catch( err ) { 
-
-        console.error( `Unable to register for membership events: ${ err }` ) 
-    };
-
-    try {
+        // Send a strategy when we're added to a new room
+        webex.memberships.on( 'created', ( event ) => sendStrategy( event ) );
     
         await webex.messages.listen();
 
-        webex.messages.on('created', (event) => {
+        // Post a new strategy on receiving any message (use @message in group spaces)
+        webex.messages.on('created', ( event ) => sendStrategy( event ) );
 
-            // Check this isn't a message we sent
-            if ( event.data.personId !== botId ) { 
+        await webex.attachmentActions.listen();
 
-                // Post a new strategy regardless of what the message to us is
-                sendStrategy( event.data.roomId );
-            }
-        })
+        // Handle adaptive card attachments events
+        webex.attachmentActions.on('created', ( event ) => handleAttachmentActions( event ) );
     }
     catch( err ) {
 
-        console.error( `Unable to register for message events: ${ err }` );
+        console.error( `Unable to register for Webex events: ${ err }` );
     }
-    
 }
 
 // Main program
 // Wrap in an async function so we can await certain operations
 (async function() {
 
+    // Use ngrok to create a public tunnel and URL
     publicUrl = await ngrok.connect( port );
 
     console.log( 'Public URL: ' + publicUrl );
 
-    try {
+    // Retrieve bot person details, i.e. id
+    let me = await webex.people.get( 'me' );
+    botId = me.id;
 
-        let webhooks = await webex.webhooks.list();
-
-        for ( let webhook in webhooks.items ) {
-
-            await webex.webhooks.remove( webhooks.items[ webhook ] )
-            .catch( ( err ) => {
-
-                console.log( err )
-            });
-        }
-    
-        let webhook = await webex.webhooks.create( 
-            {
-                resource: 'attachmentActions',
-                event: 'created',
-                targetUrl: `${ publicUrl }/webhook`,
-                name: 'Oblique Attachment Action Webhook'
-            }
-        );
-
-        botId = webhook.createdBy;
-    }
-    catch ( err ) {
-
-        console.log( `Error managing webhooks: ${ err }` );
-        process.exit( 1 );
-    }
-
-    setupWebexListeners()
-
-    // Helps parse request bodies into JSON objects
-    app.use( bodyParser.json() )
+    setupWebexListeners();
 
     // Serve static files/images from the www folder
-    app.use( express.static( 'www' ) )
-    
-    // Handler for POST requests to the /webhook path
-    app.post( '/webhook', ( req, res ) => {
+    app.use( express.static( 'www' ) );
 
-        // Retrieve the full attachment action data using the body.data.id value
-        webex.attachmentActions.get( req.body.data.id )
-        .then( ( body ) => {
-
-            switch ( body.inputs.action ) {
-
-                case 'refresh': 
-
-                    sendStrategy( req.body.data.roomId );
-                    break;
-
-                case 'settings':
-
-                    console.log( body.inputs )
-                    // Saving the settings and scheduling automatic strategy posts 
-                    // is left as an exercise for the programmer ;)
-                    break;
-            }
-        })
-        .catch( ( err ) => {
-
-            console.log( `Error getting attachment action: ${ err ? err : res.statusCode +': ' + res.statusMessage }` )
-        })
-
-        // Return HTTP 202 Accepted
-        res.status( 202 ).send();
-    })
-
-    app.listen(port, () => console.log( `Bot listening on port ${port}!` ) )
+    // Startup the Express web server
+    app.listen( port, () => console.log( `Bot listening on port ${port}!` ) );
 
 })();
 
